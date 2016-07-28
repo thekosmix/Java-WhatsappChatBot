2597fabdde040eeaa7f4f95789a46e686a6b8865
Siddharth Kumar <siddharth.dce08@gmail.com>
2016-07-29 02:00:44 +0530

    added code to read incoming messages and reply to them

--- src/main/java/in/strollup/whatsapp/main/WebWhatsapp.java
+++ src/main/java/in/strollup/whatsapp/main/WebWhatsapp.java
@@ -1,5 +1,7 @@
 package in.strollup.whatsapp.main;
 
+import in.strollup.whatsapp.utils.Utils;
+
 import java.util.List;
 import java.util.Scanner;
 
@@ -22,9 +24,14 @@ public class WebWhatsapp {
 	 */
 	private WebDriver browser = new ChromeDriver();
 	/**
-	 * user to whom you want to send the message
+	 * indicator of if a chat has been started or not
 	 */
-	private String user = "";
+	private boolean isStarted = false;
+
+	/**
+	 * time in millis to wait for checking new incoming message;
+	 */
+	private long sleepTime = 3000;
 
 	WebWhatsapp() {
 		/**
@@ -53,20 +60,69 @@ public class WebWhatsapp {
 		sc.close();
 
 		/**
-		 * get the user's span and click it to select the conversation with user
+		 * keep checking for unread count every sleepTime milli secs.
+		 * If some elementFound, then click it and set isStarted to true
+		 * The while will check for this variable, and then will reply on that element.
 		 */
-		WebElement selectedWindow = browser.findElement(By
-				.xpath("//span[contains(text(), '" + user + "')]"));
-		selectedWindow.click();
+		while (true) {
+			try {
+				if (isStarted) {
+					/**
+					 *  once the new messages are found
+					 *  get the last message and reply to the user according to it:	
+					 */
+					WebElement selectedWindow = browser.findElement(By
+							.xpath("//div[contains(@class, 'message-list')]"));
+					List<WebElement> msgList = selectedWindow.findElements(By
+							.xpath("//div[contains(@class,'msg')]"));
+					WebElement lastMsgDiv = msgList.get(msgList.size() - 1);
+					WebElement lastMsgSpan = lastMsgDiv
+							.findElement(By
+									.xpath("//span[contains(@class, 'selectable-text')]"));
+					String msg = lastMsgSpan.getText();
+					reply("I was already chatting with you: " + msg);
+					isStarted = false;
+				}
 
-		/**
-		 * After conversation is selected find the input box and enter a message
-		 * ancd click the send button
-		 */
+				/**
+				 *  get the user who just pinged, whose 'unread-count' will be 1 or more
+				 */
+				List<WebElement> nonSelectedWindows = browser.findElements(By
+						.xpath("//span[contains(@class,'unread-count')]"));
+				if (!Utils.isEmptyOrNull(nonSelectedWindows)) {
+					isStarted = true;
+					responseNonSelectedWindow(nonSelectedWindows);
+				} else {
+					Utils.log("no new msg yet");
+					Thread.sleep(sleepTime);
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+
+		browser.quit();
+	}
+
+	/**
+	 * Select the user with unread-count and click on it to start chatting with him/her
+	 * @param elems
+	 */
+	private void responseNonSelectedWindow(List<WebElement> elems) {
+		Utils.log("new msgs found");
+		for (WebElement elem : elems) {
+			elem.click();
+			reply("your chat was not selected. Now it is.");
+		}
+	}
+
+	private void reply(String string) {
 		List<WebElement> elem1 = browser.findElements(By.className("input"));
-		elem1.get(1).sendKeys("Hello World");
-		browser.findElement(By.className("send-container")).click();
-
+		for (int i = 0; i < 1; i++) {
+			elem1.get(1).sendKeys(string);//
+			browser.findElement(By.className("send-container")).click();
+		}
 	}
 
 	public static void main(String[] args) throws InterruptedException {
